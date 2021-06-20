e3cec7b9440286be3b8c9aa9f0f5ff098021ad4e
seha1 <seha1@yandex.ru>
2021-06-20 10:12:23 +0300

    feat: created adaptive menu of the site

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('js working');
+
+let btnMenuSwitch = document.getElementById('burg_line');
+
+function menuSwitch() {
+    let menu = document.getElementById('nav');
+    menu.classList.toggle('active-menu');
+}
